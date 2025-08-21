ef00f34815b249fba3063bb24d831454fdc3d957
moicben <moicben@email.com>
2025-08-22 00:28:53 +0800

    up checkout G2A

--- tests/puppeteer-profile.js
+++ tests/puppeteer-profile.js
@@ -1,18 +1,18 @@
 // tester LaunchBrowser avec userDataDir
 
 //import { proceedG2AWorkflow } from '../scripts/puppeteer/g2a/proceed.js';
-import { saveCardG2AWorkflow } from '../scripts/puppeteer/g2a/save-card.js';
+// import { saveCardG2AWorkflow } from '../scripts/puppeteer/g2a/save-card.js';
 
 
-const cardDetails = {
-  cardNumber: '4111111111111111',
-  cardExpiry: '12/2025',
-  cardCvc: '123',
-  cardHolder: 'John Doe'
-};
+// const cardDetails = {
+//   cardNumber: '4111111111111111',
+//   cardExpiry: '12/2025',
+//   cardCvc: '123',
+//   cardHolder: 'John Doe'
+// };
 
 //await proceedG2AWorkflow({ cardDetails, paymentId: "test1" });
-await saveCardG2AWorkflow({ cardDetails, paymentId: "test1" });
+//await saveCardG2AWorkflow({ cardDetails, paymentId: "test1" });
 
 
 
@@ -20,15 +20,10 @@ await saveCardG2AWorkflow({ cardDetails, paymentId: "test1" });
 
 // TEST MANUEL :
 
-// import { launchBrowser } from '../tools/puppeteer/client.js';
+import { launchBrowser } from '../tools/puppeteer/client.js';
 
-// const { browser, page } = await launchBrowser(false, false, true);
-// await page.goto('https://www.g2a.com/fr/page/cart?___locale=fr');
-// //await page.goto('https://www.google.com');
-// //await page.goto('https://www.g2a.com/fr/page/cart?___locale=fr');
-
-// await new Promise(resolve => setTimeout(resolve, 1200000));
-// await page.screenshot({ path: 'screenshot.png' });
-
-// await browser.close();
+const { browser, page } = await launchBrowser(false, false, true);
+ await page.goto('https://dashboard.g2a.com/fr/account/settings/saved-methods');
+await new Promise(resolve => setTimeout(resolve, 1200000));
+await browser.close();
 
